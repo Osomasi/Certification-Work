ecd76b2bfd9c578d7dddc0b423a908620908c714
Masi <osomasi19@gmail.com>
2022-06-09 00:18:25 +0100

    Java sctipt event listener implemented for anvigation. Practising listeners for section navigation
    Signed-off-by: Masi <osomasi19@gmail.com>

--- Project 1/project.js
+++ Project 1/project.js
@@ -3,3 +3,8 @@ const navtoggle = document.querySelector('.nav-open');
 navtoggle.addEventListener('click',()=>{
     document.body.classList.toggle('nav-slide');
 });
+
+//var servicesnav = document.getElementById('#services')
+//servicesnav.onclick = function() {};
+//servicesnav.setAttribute('onclick', 'foo()');
+//servicesnav.addEventListener('onclick', foo, false);
